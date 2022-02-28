2df963a8d17b2b6eeffd6ddcf0ec5d15db0d5a7b
Ravindu2017 <ravindu.hasanka90@gmail.com>
2022-02-28 10:48:10 +0200

    fix typos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,94 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
+//import logo from './logo.svg';
 import './App.css';
 
+
+/*This gets us the location key of the city that was typed */
+const api = {
+  key: "sGjww6q9uK6L2JVBLmLkzlJmUGBe9rF5",
+  base: "https://dataservice.accuweather.com/locations/v1/cities/search",
+  base2: "https://dataservice.accuweather.com/forecasts/v1/daily/1day/"
+}
+
+/*
+This gets the weather data
+
+const api = {
+  key: "sGjww6q9uK6L2JVBLmLkzlJmUGBe9rF5",
+  base: "http://dataservice.accuweather.com/locations/v1/cities/search"
+}
+
+http://dataservice.accuweather.com/forecasts/v1/daily/1day/{location.key}
+
+*/
+
 function App() {
+  const [query, setQuery] = useState("")
+  const [city, setCity] = useState("")
+  const [weather, setWeather] = useState({})
+
+  /*.then(response)() {
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+        return response;
+    }) */
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      try {
+        fetch(`${api.base}?apikey=${api.key}&q=${query}`)
+          .then(res => res.json())
+          .then(result => {
+            //setWeather(result)
+            //setQuery("")
+            setCity(result[0].LocalizedName)
+            console.log(weather, result[0].LocalizedName)//[0].LocalizedName)
+            return fetch(`${api.base2}${result[0].Key}?apikey=${api.key}&metric=true`)
+          })
+          .then(res => res.json())
+          .then(result => {
+            console.log(result.DailyForecasts[0])
+            setWeather(result.DailyForecasts[0])
+            setQuery("")
+          })
+      }
+      catch (err) {
+        console.log(err)
+      }
+    }
+  }
+
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
+      <main>
+        <div className="search">
+          <input type="text"
+            className="search_input"
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {(typeof weather.Temperature != "undefined") ? (
+          <div><p>City: {city}</p><p>Maximum Temperature: {weather.Temperature.Maximum.Value}</p></div>
+        ) : ("")}
+      </main>
+    </div>
+  );
+}
+
+export default App;
+
+/*
+
+  
+<header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,8 +102,5 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
-}
 
-export default App;
+*/
